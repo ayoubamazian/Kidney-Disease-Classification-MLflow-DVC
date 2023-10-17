96fb6e9a9b6a6dc8d31d07d8e84485a220a795b8
Admin <Admin@PC-ADMIN>
2023-10-17 09:03:27 +0100

    train stage done

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -21,6 +21,12 @@ class PrepareBaseModelConfig:
 
 
 @dataclass(frozen=True)
-class TrainModelConfig:
+class TrainingConfig:
     root_dir: Path
     trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
--- src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -2,6 +2,9 @@ from cnnClassifier.components.prepare_base_model import PrepareBaseModel
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.exception import CustomException
 import sys
+from cnnClassifier.logging import logging
+
+STAGE_NAME = "Prepare base model"
 
 class PrepareBaseModelPipeline:
     def __init__(self):
@@ -16,3 +19,14 @@ class PrepareBaseModelPipeline:
             prepare_base_model.updated_model()
         except Exception as e:
             raise CustomException(e, sys)
+        
+if __name__ == '__main__':
+
+    try:
+        logging.info(f"*******************")
+        logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelPipeline()
+        obj.main()
+        logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        raise CustomException(e, sys)
