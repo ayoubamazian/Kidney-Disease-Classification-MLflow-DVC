2dd14ae842145fdc22d83b5387c5be2b3e989cb8
Admin <Admin@PC-ADMIN>
2023-10-17 09:40:09 +0100

    restart the dvc for tracking

--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -2,6 +2,10 @@ from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.exception import CustomException
 import sys
+from cnnClassifier.logging import logging
+
+
+STAGE_NAME = "data ingestion stage"
 
 class DataIngestionPipeline:
     def __init__(self):
@@ -16,3 +20,16 @@ class DataIngestionPipeline:
             data_ingestion.extract_zip_file()
         except Exception as e:
             raise CustomException(e, sys)
+
+
+
+if __name__ == '__main__':
+
+    try:
+        logging.info(f"*******************")
+        logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionPipeline()
+        obj.main()
+        logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        raise CustomException(e, sys)
